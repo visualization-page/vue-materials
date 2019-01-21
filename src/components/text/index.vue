a88f9d6179a4d90949d8baa5c7418b32413c6098
yangming <472102644@qq.com>
2019-01-21 15:48:49 +0800

    fix

---
+++ src/components/text/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ config.text }}</p>
+</template>
+
+<script>
+export default {
+  name: 'ytext',
+
+  props: {
+    config: {
+      type: Object,
+      required: true,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
